f80ca76e1aae72ac8155041ba33ad46c56d9f0de
Vicky Gupta <44772160+vicky60629@users.noreply.github.com>
2026-02-19 01:04:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+import pickle
 import joblib
 
 app = Flask(__name__)
-model = joblib.load('model.pkl')
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
 
 @app.route('/')
 def home():
@@ -12,58 +15,21 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
 
-    int_features = [float(x) for x in request.form.values()]
+    int_features = [int(x) for x in request.form.values()]
 
-    if int_features[0]==0:
-        f_features=[0,0,0]+int_features[1:]
-    elif int_features[0]==1:
-        f_features=[1,0,0]+int_features[1:]
-    elif int_features[0]==2:
-        f_features=[0,1,0]+int_features[1:]
-    else:
-        f_features=[0,0,1]+int_features[1:]
+    int_features[4]=np.log(int_features[4])
+    int_features[5]=np.log(int_features[5])
+    int_features[6]=np.log(int_features[6])
+    int_features[7]=np.log(int_features[7])
+    int_features[8]=np.log(int_features[8])
+    int_features[9]=np.log(int_features[9])
+    int_features[10]=np.log(int_features[10])
+    int_features[11]=np.sqrt(int_features[11])
+    int_features[12]=np.sqrt(int_features[12])
+    int_features[13]=np.sqrt(int_features[13])
+    int_features[14]=np.square(int_features[14])
 
-    if f_features[6]==0:
-        fn_features=f_features[:6]+[0,0]+f_features[7:]
-    elif f_features[6]==1:
-        fn_features=f_features[:6]+[1,0]+f_features[7:]
-    else:
-        fn_features=f_features[:6]+[0,1]+f_features[7:]
 
-    final_features = [np.array(fn_features)]
-    predict = model.predict(final_features)
-
-    if predict==0:
-        output='Normal'
-    elif predict==1:
-        output='DOS'
-    elif predict==2:
-        output='PROBE'
-    elif predict==3:
-        output='R2L'
-    else:
-        output='U2R'
-
-    return render_template('index.html', output=output)
-
-@app.route('/results',methods=['POST'])
-def results():
-
-    data = request.get_json(force=True)
-    predict = model.predict([np.array(list(data.values()))])
-
-    if predict==0:
-        output='Normal'
-    elif predict==1:
-        output='DOS'
-    elif predict==2:
-        output='PROBE'
-    elif predict==3:
-        output='R2L'
-    else:
-        output='U2R'
-
-    return jsonify(output)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
